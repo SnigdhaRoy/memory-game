d9798cb1c62a958d50bc47012641aa8606cd231f
SnigdhaRoy <51500061+SnigdhaRoy@users.noreply.github.com>
2019-06-07 23:03:16 -0400

    Add files via upload

---
+++ Memory Game.Designer.vb
@@ -0,0 +1,374 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.picbx16 = New System.Windows.Forms.PictureBox()
+        Me.picbx13 = New System.Windows.Forms.PictureBox()
+        Me.picbx14 = New System.Windows.Forms.PictureBox()
+        Me.picbx15 = New System.Windows.Forms.PictureBox()
+        Me.picbx11 = New System.Windows.Forms.PictureBox()
+        Me.picbx10 = New System.Windows.Forms.PictureBox()
+        Me.picbx9 = New System.Windows.Forms.PictureBox()
+        Me.picbx12 = New System.Windows.Forms.PictureBox()
+        Me.picbx7 = New System.Windows.Forms.PictureBox()
+        Me.picbx6 = New System.Windows.Forms.PictureBox()
+        Me.picbx5 = New System.Windows.Forms.PictureBox()
+        Me.picbx8 = New System.Windows.Forms.PictureBox()
+        Me.picbx2 = New System.Windows.Forms.PictureBox()
+        Me.picbx4 = New System.Windows.Forms.PictureBox()
+        Me.picbx3 = New System.Windows.Forms.PictureBox()
+        Me.picbx1 = New System.Windows.Forms.PictureBox()
+        Me.picbx19 = New System.Windows.Forms.PictureBox()
+        Me.picbx18 = New System.Windows.Forms.PictureBox()
+        Me.picbx17 = New System.Windows.Forms.PictureBox()
+        Me.picbx20 = New System.Windows.Forms.PictureBox()
+        Me.picbx23 = New System.Windows.Forms.PictureBox()
+        Me.picbx22 = New System.Windows.Forms.PictureBox()
+        Me.picbx21 = New System.Windows.Forms.PictureBox()
+        Me.picbx24 = New System.Windows.Forms.PictureBox()
+        CType(Me.picbx16, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picbx13, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picbx14, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picbx15, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picbx11, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picbx10, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picbx9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picbx12, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picbx7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picbx6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picbx5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picbx8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picbx2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picbx4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picbx3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picbx1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picbx19, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picbx18, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picbx17, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picbx20, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picbx23, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picbx22, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picbx21, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picbx24, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picbx16
+        '
+        Me.picbx16.Location = New System.Drawing.Point(438, 201)
+        Me.picbx16.Name = "picbx16"
+        Me.picbx16.Size = New System.Drawing.Size(73, 36)
+        Me.picbx16.TabIndex = 0
+        Me.picbx16.TabStop = False
+        Me.picbx16.Tag = "16"
+        '
+        'picbx13
+        '
+        Me.picbx13.Location = New System.Drawing.Point(25, 201)
+        Me.picbx13.Name = "picbx13"
+        Me.picbx13.Size = New System.Drawing.Size(73, 36)
+        Me.picbx13.TabIndex = 1
+        Me.picbx13.TabStop = False
+        Me.picbx13.Tag = "13"
+        '
+        'picbx14
+        '
+        Me.picbx14.Location = New System.Drawing.Point(168, 201)
+        Me.picbx14.Name = "picbx14"
+        Me.picbx14.Size = New System.Drawing.Size(73, 36)
+        Me.picbx14.TabIndex = 2
+        Me.picbx14.TabStop = False
+        Me.picbx14.Tag = "14"
+        '
+        'picbx15
+        '
+        Me.picbx15.Location = New System.Drawing.Point(304, 201)
+        Me.picbx15.Name = "picbx15"
+        Me.picbx15.Size = New System.Drawing.Size(73, 36)
+        Me.picbx15.TabIndex = 3
+        Me.picbx15.TabStop = False
+        Me.picbx15.Tag = "15"
+        '
+        'picbx11
+        '
+        Me.picbx11.Location = New System.Drawing.Point(304, 133)
+        Me.picbx11.Name = "picbx11"
+        Me.picbx11.Size = New System.Drawing.Size(73, 36)
+        Me.picbx11.TabIndex = 7
+        Me.picbx11.TabStop = False
+        Me.picbx11.Tag = "11"
+        '
+        'picbx10
+        '
+        Me.picbx10.Location = New System.Drawing.Point(168, 133)
+        Me.picbx10.Name = "picbx10"
+        Me.picbx10.Size = New System.Drawing.Size(73, 36)
+        Me.picbx10.TabIndex = 6
+        Me.picbx10.TabStop = False
+        Me.picbx10.Tag = "10"
+        '
+        'picbx9
+        '
+        Me.picbx9.Location = New System.Drawing.Point(25, 133)
+        Me.picbx9.Name = "picbx9"
+        Me.picbx9.Size = New System.Drawing.Size(73, 36)
+        Me.picbx9.TabIndex = 5
+        Me.picbx9.TabStop = False
+        Me.picbx9.Tag = "9"
+        '
+        'picbx12
+        '
+        Me.picbx12.Location = New System.Drawing.Point(438, 133)
+        Me.picbx12.Name = "picbx12"
+        Me.picbx12.Size = New System.Drawing.Size(73, 36)
+        Me.picbx12.TabIndex = 4
+        Me.picbx12.TabStop = False
+        Me.picbx12.Tag = "12"
+        '
+        'picbx7
+        '
+        Me.picbx7.Location = New System.Drawing.Point(304, 66)
+        Me.picbx7.Name = "picbx7"
+        Me.picbx7.Size = New System.Drawing.Size(73, 36)
+        Me.picbx7.TabIndex = 11
+        Me.picbx7.TabStop = False
+        Me.picbx7.Tag = "7"
+        '
+        'picbx6
+        '
+        Me.picbx6.Location = New System.Drawing.Point(168, 66)
+        Me.picbx6.Name = "picbx6"
+        Me.picbx6.Size = New System.Drawing.Size(73, 36)
+        Me.picbx6.TabIndex = 10
+        Me.picbx6.TabStop = False
+        Me.picbx6.Tag = "6"
+        '
+        'picbx5
+        '
+        Me.picbx5.Location = New System.Drawing.Point(25, 66)
+        Me.picbx5.Name = "picbx5"
+        Me.picbx5.Size = New System.Drawing.Size(73, 36)
+        Me.picbx5.TabIndex = 9
+        Me.picbx5.TabStop = False
+        Me.picbx5.Tag = "5"
+        '
+        'picbx8
+        '
+        Me.picbx8.Location = New System.Drawing.Point(438, 66)
+        Me.picbx8.Name = "picbx8"
+        Me.picbx8.Size = New System.Drawing.Size(73, 36)
+        Me.picbx8.TabIndex = 8
+        Me.picbx8.TabStop = False
+        Me.picbx8.Tag = "8"
+        '
+        'picbx2
+        '
+        Me.picbx2.Location = New System.Drawing.Point(168, 10)
+        Me.picbx2.Name = "picbx2"
+        Me.picbx2.Size = New System.Drawing.Size(73, 36)
+        Me.picbx2.TabIndex = 14
+        Me.picbx2.TabStop = False
+        Me.picbx2.Tag = "2"
+        '
+        'picbx4
+        '
+        Me.picbx4.Location = New System.Drawing.Point(438, 10)
+        Me.picbx4.Name = "picbx4"
+        Me.picbx4.Size = New System.Drawing.Size(73, 36)
+        Me.picbx4.TabIndex = 19
+        Me.picbx4.TabStop = False
+        Me.picbx4.Tag = "4"
+        '
+        'picbx3
+        '
+        Me.picbx3.Location = New System.Drawing.Point(304, 10)
+        Me.picbx3.Name = "picbx3"
+        Me.picbx3.Size = New System.Drawing.Size(73, 36)
+        Me.picbx3.TabIndex = 18
+        Me.picbx3.TabStop = False
+        Me.picbx3.Tag = "3"
+        '
+        'picbx1
+        '
+        Me.picbx1.Location = New System.Drawing.Point(25, 10)
+        Me.picbx1.Name = "picbx1"
+        Me.picbx1.Size = New System.Drawing.Size(73, 36)
+        Me.picbx1.TabIndex = 17
+        Me.picbx1.TabStop = False
+        Me.picbx1.Tag = "1"
+        '
+        'picbx19
+        '
+        Me.picbx19.Location = New System.Drawing.Point(304, 257)
+        Me.picbx19.Name = "picbx19"
+        Me.picbx19.Size = New System.Drawing.Size(73, 36)
+        Me.picbx19.TabIndex = 23
+        Me.picbx19.TabStop = False
+        Me.picbx19.Tag = "19"
+        '
+        'picbx18
+        '
+        Me.picbx18.Location = New System.Drawing.Point(168, 257)
+        Me.picbx18.Name = "picbx18"
+        Me.picbx18.Size = New System.Drawing.Size(73, 36)
+        Me.picbx18.TabIndex = 22
+        Me.picbx18.TabStop = False
+        Me.picbx18.Tag = "18"
+        '
+        'picbx17
+        '
+        Me.picbx17.Location = New System.Drawing.Point(25, 257)
+        Me.picbx17.Name = "picbx17"
+        Me.picbx17.Size = New System.Drawing.Size(73, 36)
+        Me.picbx17.TabIndex = 21
+        Me.picbx17.TabStop = False
+        Me.picbx17.Tag = "17"
+        '
+        'picbx20
+        '
+        Me.picbx20.Location = New System.Drawing.Point(438, 257)
+        Me.picbx20.Name = "picbx20"
+        Me.picbx20.Size = New System.Drawing.Size(73, 36)
+        Me.picbx20.TabIndex = 20
+        Me.picbx20.TabStop = False
+        Me.picbx20.Tag = "20"
+        '
+        'picbx23
+        '
+        Me.picbx23.Location = New System.Drawing.Point(304, 308)
+        Me.picbx23.Name = "picbx23"
+        Me.picbx23.Size = New System.Drawing.Size(73, 36)
+        Me.picbx23.TabIndex = 27
+        Me.picbx23.TabStop = False
+        Me.picbx23.Tag = "23"
+        '
+        'picbx22
+        '
+        Me.picbx22.Location = New System.Drawing.Point(168, 308)
+        Me.picbx22.Name = "picbx22"
+        Me.picbx22.Size = New System.Drawing.Size(73, 36)
+        Me.picbx22.TabIndex = 26
+        Me.picbx22.TabStop = False
+        Me.picbx22.Tag = "22"
+        '
+        'picbx21
+        '
+        Me.picbx21.Location = New System.Drawing.Point(25, 308)
+        Me.picbx21.Name = "picbx21"
+        Me.picbx21.Size = New System.Drawing.Size(73, 36)
+        Me.picbx21.TabIndex = 25
+        Me.picbx21.TabStop = False
+        Me.picbx21.Tag = "21"
+        '
+        'picbx24
+        '
+        Me.picbx24.Location = New System.Drawing.Point(438, 308)
+        Me.picbx24.Name = "picbx24"
+        Me.picbx24.Size = New System.Drawing.Size(73, 36)
+        Me.picbx24.TabIndex = 24
+        Me.picbx24.TabStop = False
+        Me.picbx24.Tag = "24"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.picbx23)
+        Me.Controls.Add(Me.picbx22)
+        Me.Controls.Add(Me.picbx21)
+        Me.Controls.Add(Me.picbx24)
+        Me.Controls.Add(Me.picbx19)
+        Me.Controls.Add(Me.picbx18)
+        Me.Controls.Add(Me.picbx17)
+        Me.Controls.Add(Me.picbx20)
+        Me.Controls.Add(Me.picbx4)
+        Me.Controls.Add(Me.picbx3)
+        Me.Controls.Add(Me.picbx1)
+        Me.Controls.Add(Me.picbx2)
+        Me.Controls.Add(Me.picbx7)
+        Me.Controls.Add(Me.picbx6)
+        Me.Controls.Add(Me.picbx5)
+        Me.Controls.Add(Me.picbx8)
+        Me.Controls.Add(Me.picbx11)
+        Me.Controls.Add(Me.picbx10)
+        Me.Controls.Add(Me.picbx9)
+        Me.Controls.Add(Me.picbx12)
+        Me.Controls.Add(Me.picbx15)
+        Me.Controls.Add(Me.picbx14)
+        Me.Controls.Add(Me.picbx13)
+        Me.Controls.Add(Me.picbx16)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.picbx16, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picbx13, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picbx14, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picbx15, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picbx11, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picbx10, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picbx9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picbx12, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picbx7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picbx6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picbx5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picbx8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picbx2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picbx4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picbx3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picbx1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picbx19, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picbx18, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picbx17, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picbx20, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picbx23, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picbx22, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picbx21, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picbx24, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents picbx16 As PictureBox
+    Friend WithEvents picbx13 As PictureBox
+    Friend WithEvents picbx14 As PictureBox
+    Friend WithEvents picbx15 As PictureBox
+    Friend WithEvents picbx11 As PictureBox
+    Friend WithEvents picbx10 As PictureBox
+    Friend WithEvents picbx9 As PictureBox
+    Friend WithEvents picbx12 As PictureBox
+    Friend WithEvents picbx7 As PictureBox
+    Friend WithEvents picbx6 As PictureBox
+    Friend WithEvents picbx5 As PictureBox
+    Friend WithEvents picbx8 As PictureBox
+    Friend WithEvents picbx2 As PictureBox
+    Friend WithEvents picbx4 As PictureBox
+    Friend WithEvents picbx3 As PictureBox
+    Friend WithEvents picbx1 As PictureBox
+    Friend WithEvents picbx19 As PictureBox
+    Friend WithEvents picbx18 As PictureBox
+    Friend WithEvents picbx17 As PictureBox
+    Friend WithEvents picbx20 As PictureBox
+    Friend WithEvents picbx23 As PictureBox
+    Friend WithEvents picbx22 As PictureBox
+    Friend WithEvents picbx21 As PictureBox
+    Friend WithEvents picbx24 As PictureBox
+End Class
---
+++ Memory Game.vb
@@ -0,0 +1,127 @@
+﻿'Snigdha Roy final project VB June 2019'
+Public Class Form1
+    Dim intrndone As Integer
+    Dim intrndtwo As Integer
+    Dim intloop As Integer
+    Dim intboard(23) As Integer
+    Dim intcount As Integer
+    Dim oldtag As Integer
+    Dim oldpic As PictureBox
+    Dim intnomatches As Integer
+    Dim intnopoint As Integer = 0
+    Dim intmatch As Integer
+    Dim lstindexitems As Integer
+    Dim picboxsave As PictureBox
+    'initializing variables
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'loads all cards to be the same from the back
+        picbx1.Image = Image.FromFile("cover.jpg")
+        picbx2.Image = Image.FromFile("cover.jpg")
+        picbx3.Image = Image.FromFile("cover.jpg")
+        picbx4.Image = Image.FromFile("cover.jpg")
+        picbx5.Image = Image.FromFile("cover.jpg")
+        picbx6.Image = Image.FromFile("cover.jpg")
+        picbx7.Image = Image.FromFile("cover.jpg")
+        picbx8.Image = Image.FromFile("cover.jpg")
+        picbx9.Image = Image.FromFile("cover.jpg")
+        picbx10.Image = Image.FromFile("cover.jpg")
+        picbx11.Image = Image.FromFile("cover.jpg")
+        picbx12.Image = Image.FromFile("cover.jpg")
+        picbx13.Image = Image.FromFile("cover.jpg")
+        picbx14.Image = Image.FromFile("cover.jpg")
+        picbx15.Image = Image.FromFile("cover.jpg")
+        picbx16.Image = Image.FromFile("cover.jpg")
+        picbx17.Image = Image.FromFile("cover.jpg")
+        picbx18.Image = Image.FromFile("cover.jpg")
+        picbx19.Image = Image.FromFile("cover.jpg")
+        picbx20.Image = Image.FromFile("cover.jpg")
+        picbx21.Image = Image.FromFile("cover.jpg")
+        picbx22.Image = Image.FromFile("cover.jpg")
+        picbx23.Image = Image.FromFile("cover.jpg")
+        picbx24.Image = Image.FromFile("cover.jpg")
+
+        'additionally randomizing numbers to eliminate repetition
+        'randomizing to set cards to pictures
+
+        Randomize()
+
+        For intloop = 1 To 12
+            Do
+                intrndone = Int(Rnd() * 24 + 1)
+
+            Loop While (intboard(intrndone) <> 0)
+            'if empty, keep random number
+            'if not empty, it will keep looping
+
+            Do
+                intrndtwo = Int(Rnd() * 24 + 1)
+
+            Loop While ((intrndtwo = intrndone) Or (intboard(intrndtwo - 1) <> 0))
+            intboard(intrndone - 1) = intloop
+            intboard(intrndtwo - 1) = intloop
+        Next intloop
+    End Sub
+
+    Private Sub pic_Click(sender As Object, e As EventArgs) Handles picbx1.Click, picbx2.Click,
+            picbx3.Click, picbx4.Click, picbx5.Click, picbx6.Click, picbx7.Click, picbx8.Click,
+            picbx9.Click, picbx10.Click, picbx11.Click, picbx12.Click, picbx13.Click, picbx14.Click,
+            picbx15.Click, picbx16.Click, picbx17.Click, picbx18.Click, picbx19.Click, picbx20.Click,
+            picbx21.Click, picbx22.Click, picbx23.Click, picbx24.Click
+
+        Dim pic As PictureBox = sender
+        Dim inttag As Integer = pic.Tag
+
+        'determines which pic will match each picture box depending on tag
+
+        If intboard(inttag) = 1 Then
+            pic.Image = Image.FromFile("bulbasaur.jpg")
+        End If
+
+        If intboard(inttag) = 2 Then
+            pic.Image = Image.FromFile("venasaur.png")
+        End If
+
+        If intboard(inttag) = 3 Then
+            pic.Image = Image.FromFile("squirtle.png")
+        End If
+
+        If intboard(inttag) = 4 Then
+            pic.Image = Image.FromFile("blastoise.png")
+        End If
+
+        If intboard(inttag) = 5 Then
+            pic.Image = Image.FromFile("charmander.png")
+        End If
+
+        If intboard(inttag) = 6 Then
+            pic.Image = Image.FromFile("charizard.png")
+        End If
+
+        If intboard(inttag) = 7 Then
+            pic.Image = Image.FromFile("froakie.png")
+        End If
+
+        If intboard(inttag) = 8 Then
+            pic.Image = Image.FromFile("greninja.png")
+        End If
+
+        If intboard(inttag) = 9 Then
+            pic.Image = Image.FromFile("chesnaught.png")
+        End If
+
+        If intboard(inttag) = 10 Then
+            pic.Image = Image.FromFile("fennekin.png")
+        End If
+
+        If intboard(inttag) = 11 Then
+            pic.Image = Image.FromFile("delphox.jpg")
+        End If
+
+        If intboard(inttag) = 12 Then
+            pic.Image = Image.FromFile("pikachu.jpg")
+        End If
+
+
+    End Sub
+End Class
